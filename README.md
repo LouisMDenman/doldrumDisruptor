0fe07c3cef4af2b2944257c5a5bd229d220af02b
Louis Denman <lmdenman02@gmail.com>
2024-10-07 14:49:02 +1000

    added early wireframe mockup to assets section, and edited app.jsx along with index.css and app.css to create app layout that resembles this mockup

--- README.md
+++ README.md
@@ -7,3 +7,7 @@ The word "doldrums" refers to a state or period stagnation or depression, while 
 ## Why Doldrum Disruptor?
 
 This year I have personally been trying to refrain from immediately scrolling on socia media apps via my phone as soon as I get bored, especially when I am with my partner, as I quickly become disengaged and we end up spending less quality time together. As a result, I have found that there is no longer a quick fix to my boredom, which I believe is a good thing as it challenges me to come up with new ways to pass time. Since I am studying this year and don't have as much disposible income as I did when I was working, however, I find it difficult sometimes to come up with creative and cost-effective pastimes. Because of this, I came up with the idea to create a project that can give me ideas for things to do both by myself and with my partner/others, that also matches the constraints mentioned previously.
+
+## Inital Rough Wireframe
+
+![Doldrum Disruptor Early Wireframe](./src/assets/DoldrumDisruptorWireframe.png)
--- src/App.jsx
+++ src/App.jsx
@@ -7,10 +7,56 @@ function App() {
 
   return (
     <>
-      <h1>Doldrum Disruptor</h1>
-      <button onClick={toggleTheme}>
-        Toggle Theme
-      </button>
+      <header>
+        <h1>Doldrum Disruptor</h1>
+      </header>
+      <main>
+        <div id="output">
+          <p>PLACEHOLDER: OUTPUT WILL GO HERE</p>
+        </div>
+        <div id="checkboxes">
+          <div>
+            <input type="checkbox" id="solo" name="solo"/>
+            <label for="solo">Solo Activity</label>
+          </div>
+          <div>
+            <input type="checkbox" id="social" name="social"/>
+            <label for="social">Social</label>
+          </div>
+          <div>
+            <input type="checkbox" id="recreational" name="recreational"/>
+            <label for="recreational">Recreational</label>
+          </div>
+          <div>
+            <input type="checkbox" id="educational" name="educational"/>
+            <label for="educational">Educational</label>
+          </div>
+          <div>
+            <input type="checkbox" id="relaxing" name="relaxing"/>
+            <label for="relaxing">Relaxing</label>
+          </div>
+          <div>
+            <input type="checkbox" id="charitable" name="charitable"/>
+            <label for="charitable">Charitable</label>
+          </div>
+          <div>
+            <input type="checkbox" id="cooking" name="cooking"/>
+            <label for="cooking">Cooking</label>
+          </div>
+          <div>
+            <input type="checkbox" id="work" name="work"/>
+            <label for="work">Work</label>
+          </div>
+        </div>
+        <button id="search">
+          Find Activity
+        </button>
+      </main>
+      <footer>
+        <button id="theme" onClick={toggleTheme}>
+          Toggle Theme
+        </button>
+      </footer>
     </>
   )
 }
