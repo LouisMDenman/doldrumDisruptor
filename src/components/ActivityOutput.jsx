290cc756f1d90e0c5f160afdd23732849c4ee609
Louis Denman <lmdenman02@gmail.com>
2024-10-10 11:41:28 +1000

    sought and found external fix for CORS issues, and implemented basic API call functionality

--- src/components/ActivityOutput.jsx
+++ src/components/ActivityOutput.jsx
@@ -1,26 +1,75 @@
+import { useState } from "react"
 
-const getRandomData = async () => {
-    const response = await fetch("https://bored-api.appbrewery.com/random")
-    console.log(response)
-}
 
-const onClickHandler = () => {
-    getRandomData()
-}
 
 export function ActivityOutput() {
+    const [error, setError] = useState(null)
+    const [id, setId] = useState(null)
+    const [activity, setActivity] = useState(null)
+    const [category, setCategory] = useState(null)
+    const [duration, setDuration] = useState(null)
+    const [kidFriendly, setKidFriendly] = useState(null)
+
+    const getRandomData = async () => {
+        const response = await fetch("https://bored-api.appbrewery.com/random", {
+            methods: "GET"
+        })
+        if (response.ok) {
+            const body = await response.json()
+            console.log(body)
+            return body
+        }
+        else {
+            console.error('Promise resolved but HTTP status failed')
+            const body = {"error": "Aww man, unfortunately the bored API this website uses only takes 100 requests every 15 minutes. You'll just have to be bored for a bit longer!"}
+            return body
+        }
+    }
     
-    if (false) {
+    const onClickHandler = async () => {
+        const body = await getRandomData()
+        if (body.error) {
+            setError(body.error)
+        }
+        else {
+            setId(body.key)
+            setActivity(body.activity)
+            setCategory(body.type)
+            setDuration(body.duration)
+            if (body.kidFriendly) {
+                setKidFriendly("Yes")
+            }
+            else {
+                setKidFriendly("No")
+            }
+        }
+    }
+    
+    if (error) {
         return (
             <>
                 <div id="output">
-                    <h3 id="activity">Activity: {activityData.activity}</h3>
-                    <h3 id="type">Category: {activityData.type}</h3>
-                    <h3 id="duration">Expected Duration: {activityData.duration}</h3>
-                    <h3 id="kidfriendly">Child Friendly: {activityData.kidFriendly}</h3>
+                    <h3 id="error">Error: {error}</h3>
                 </div>
                 <div id="buttondiv">
-                    <button id="search">
+                    <button id="search" onClick={onClickHandler}>
+                        Find Activity
+                    </button>
+                </div>
+            </>
+        )
+    }
+    else if (id) {
+        return (
+            <>
+                <div id="output">
+                    <h3 id="activity">Activity: {activity}</h3>
+                    <h3 id="type">Category: {category}</h3>
+                    <h3 id="duration">Expected Duration: {duration}</h3>
+                    <h3 id="kidfriendly">Child Friendly: {kidFriendly}</h3>
+                </div>
+                <div id="buttondiv">
+                    <button id="search" onClick={onClickHandler}>
                         Find Activity
                     </button>
                 </div>
