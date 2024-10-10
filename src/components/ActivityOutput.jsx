26f47297cba12e2979e9a5e9b554af5865a41ae5
Louis Denman <lmdenman02@gmail.com>
2024-10-10 14:25:58 +1000

    added code comments to more complex code and cleaned up file formatting

--- src/components/ActivityOutput.jsx
+++ src/components/ActivityOutput.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react"
 
+//Support function for generating a random integer between a min and max value inclusive
 const getRandomInteger = (min, max) => {
     min = Math.ceil(min)
     max = Math.floor(max)
@@ -7,7 +8,9 @@ const getRandomInteger = (min, max) => {
     return Math.floor(Math.random() * (max - min)) + min
   }
 
+//Main component function
 export function ActivityOutput() {
+    //State variables required and their setters declared here
     const [error, setError] = useState(null)
     const [id, setId] = useState(null)
     const [activity, setActivity] = useState(null)
@@ -15,30 +18,35 @@ export function ActivityOutput() {
     const [duration, setDuration] = useState(null)
     const [kidFriendly, setKidFriendly] = useState(null)
 
+    //Asyncronous function responsible for fetching the API data
     const getRandomData = async () => {
+        //Declare variables for the two select element values category and participants
         const selectParticipants = document.getElementById("selectparticipants")
         const selectCategory = document.getElementById("selectcategory")
-        console.log(selectParticipants.value)
-        console.log(selectCategory.value)
 
+        //Call the API and await a response using the values in the category and participants selectors
         const response = await fetch(`https://bored-api.appbrewery.com/filter?participants=${selectParticipants.value}&type=${selectCategory.value}`, {
             methods: "GET"
         })
         if (response.ok) {
+            //Wait to recieve the list of objects as JSON
             const largeBody = await response.json()
+            //Select a random JSON object from the list of objects
             const body = largeBody[getRandomInteger(0,largeBody.length)]
-            console.log(body)
             return body
         }
         else {
+            //Block to display when there isn't an activity for the current selector combination
             if (response.status == "404") {
                 const body = {"error": "Oh dear, it looks like there are no activities currently available for this number of participants in this category! Try swapping one of these for results."}
                 return body
             }
+            //Block to display when the API has recieved too many requests in too short of a timeframe
             else if (response.status == "429") {
                 const body = {"error": "Aww man, unfortunately the bored API this website uses only takes 100 requests every 15 minutes. You'll just have to be bored for a bit longer!"}
                 return body
             }
+            //Block for any other unanticipated errors
             else {
                 const body = {"error": "An unexpected error occured."}
                 console.log(response.status)
@@ -47,18 +55,24 @@ export function ActivityOutput() {
         }
     }
     
+    //Asyncronous function that is called when the find activity button is clicked
     const onClickHandler = async () => {
+        //Set the error and id state variables as null, so that they don't trigger a return option below even though there wasn't a new request
         setError(null)
         setId(null)
+        //Call the getRandomData function to get the API data
         const body = await getRandomData()
+        //if getRandomData returns an error, set the error in state
         if (body.error) {
             setError(body.error)
         }
+        //set the other values in state
         else {
             setId(body.key)
             setActivity(body.activity)
             setCategory(body.type)
             setDuration(body.duration)
+            //kidFriendly returns a true or false value, so dependant on this make the kidFriendly part of the object "yes" or "no"
             if (body.kidFriendly) {
                 setKidFriendly("Yes")
             }
@@ -68,6 +82,7 @@ export function ActivityOutput() {
         }
     }
     
+    //if the error state exists
     if (error) {
         return (
             <>
@@ -82,6 +97,7 @@ export function ActivityOutput() {
             </>
         )
     }
+    //if the id state exists
     else if (id) {
         return (
             <>
@@ -100,7 +116,9 @@ export function ActivityOutput() {
 
         )
     }
+    //if neither of those states exist
     else {
+        //if there is not a previous activity in state
         if (!activity) {
             return (
                 <>
--- src/components/ActivitySelectors.jsx
+++ src/components/ActivitySelectors.jsx
@@ -1,5 +1,3 @@
-
-
 export function ActivitySelectors() {
     return (
         <div id="selectors">
